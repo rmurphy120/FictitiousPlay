551f7b8bfabc85baf6539d6b2c0ca59715f3075d
rmurphy120 <ryno.murphy1@gmail.com>
2025-03-08 20:43:51 -0600

    Finished Q planning

--- src/Driver.java
+++ src/Driver.java
@@ -3,6 +3,7 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -11,15 +12,35 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.*;
 
 public class Driver extends Application {
     // Directory where the images are pulled
     public final static File[] IMAGE_FILES = new File("images").listFiles();
     // Size of the app. Number represents pixel length of one tile
     public static final int SIZE = 128;
+    // If SIM is false, runs Q planning and saves the results. If SIM is true, runs a simulation of the cars
+    private static final boolean SIM = true;
+
+    public static final ImageView[] CARS = new ImageView[] {
+            getImageView("redCar.png"),
+            getImageView("yellowCar.png"),
+            getImageView("blueCar.png")};
+    public static State state;
 
     public static void main(String[] args) {
-        Application.launch(args);
+        if (!SIM) {
+            MarkovGame.qPlanning();
+            // MarkovGame.saveStatesAsObject("nash_equilibrium.ser");
+            MarkovGame.saveStatesAsObject("nash_equilibrium.csv");
+        } else {
+            // MarkovGame.loadStatesAsObject("nash_equilibrium.ser");
+            MarkovGame.loadStatesAsObject("nash_equilibrium.csv");
+            state = MarkovGame.states[State.getStateIndex(
+                    new int[] {1, 1, 0, 0, MarkovGame.WIDTH - 1, MarkovGame.HEIGHT - 1})];
+            Application.launch(args);
+        }
     }
 
     @Override
@@ -30,28 +51,52 @@ public class Driver extends Application {
 
         // Create a timeline that updates every second
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
-            // Currently just makes a random move
-            for (Car each : Board.CARS)
-                each.moveRandomly();
+            ActionSpace[] actions = new ActionSpace[MarkovGame.NUM_AGENTS];
+
+            for (int a = 0; a < MarkovGame.NUM_AGENTS; a++) {
+                actions[a] = MarkovGame.getMoveFromPolicy(state.NE[a]);
+                switch (actions[a]) {
+                    case UP -> CARS[a].setY(CARS[a].getY() - SIZE);
+                    case DOWN -> CARS[a].setY(CARS[a].getY() + SIZE);
+                    case LEFT -> CARS[a].setX(CARS[a].getX() - SIZE);
+                    case RIGHT -> CARS[a].setX(CARS[a].getX() + SIZE);
+                }
+            }
+
+            state = state.transition(actions);
         }));
 
         // Run timeline indefinitely
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
 
-        Scene scene = new Scene(root, Board.WIDTH * SIZE, Board.HEIGHT * SIZE);
+        Scene scene = new Scene(root, MarkovGame.WIDTH * SIZE, MarkovGame.HEIGHT * SIZE);
 
         window.setTitle("Car Game");
         window.setScene(scene);
         window.show();
     }
 
+    private static ImageView getImageView(String spriteName) {
+        try {
+            for (File imageFile : Driver.IMAGE_FILES)
+                if (imageFile.getName().equals(spriteName)) {
+                    FileInputStream inStream = new FileInputStream(imageFile);
+                    return new ImageView(new Image(inStream));
+                }
+        } catch (Exception e) {
+            System.out.println(e.getClass() + ": " + e.getMessage());
+        }
+
+        throw new NoSuchElementException("Sprite not initialized");
+    }
+
     private void createBoard(Pane root) {
         Group boardTiles = new Group();
 
         // Creates and draws the checkered grid based off of HEIGHT and WIDTH
-        for (int y = 0; y < Board.HEIGHT; y++)
-            for (int x = 0; x < Board.WIDTH; x++) {
+        for (int y = 0; y < MarkovGame.HEIGHT; y++)
+            for (int x = 0; x < MarkovGame.WIDTH; x++) {
                 Rectangle tile = new Rectangle(x * SIZE, y * SIZE, SIZE, SIZE);
 
                 tile.setFill((x + y) % 2 == 0 ? Color.BISQUE : Color.SADDLEBROWN);
@@ -63,17 +108,13 @@ public class Driver extends Application {
 
         root.getChildren().add(boardTiles);
 
-        // Draws cars
-        for (Car each : Board.CARS) {
-            ImageView sprite = each.getImageView();
-
-            sprite.setX(each.getX() * SIZE);
-            sprite.setY(each.getY() * SIZE);
-
-            sprite.setFitHeight(SIZE);
-            sprite.setFitWidth(SIZE);
-
-            root.getChildren().add(sprite);
+        // Draws the cars based off the state
+        for (int a = 0; a < MarkovGame.NUM_AGENTS; a++) {
+            CARS[a].setX(state.state[2*a] * SIZE);
+            CARS[a].setY(state.state[2*a+1] * SIZE);
+            CARS[a].setFitWidth(SIZE);
+            CARS[a].setFitHeight(SIZE);
+            root.getChildren().add(CARS[a]);
         }
     }
 }
